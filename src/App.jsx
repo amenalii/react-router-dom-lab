5d599699103c8f4b7a0249a307befabadb1a34bf
amenalii <ummeamen@gmail.com>
2025-04-02 02:02:34 -0500

    NavBar Creation + Addition of other component folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
-import { useState } from 'react'
 import './App.css'
+import { useState } from 'react'
+import NavBar from './components/NavBar/NavBar';
+
 
 const App = () => {
-  return <h1>Hello world!</h1>;
+  return (
+    <>
+    <NavBar />
+    <h1>Post Office</h1>
+    </>
+  )
 };
 
 export default App;
